6c8416191052a95b6b67494cdeb86313bc4bda58
Martin Cook <33993749+martycook@users.noreply.github.com>
2024-12-27 10:23:46 -0600

    I added additional exception types.

---
+++ src/CodeGator.Exceptions/ApiException.cs
@@ -0,0 +1,55 @@
+﻿
+namespace CodeGator.Exceptions;
+
+/// <summary>
+/// This class represents an API related exception.
+/// </summary>
+public class ApiException : Exception
+{
+    // *******************************************************************
+    // Constructors.
+    // *******************************************************************
+
+    #region Constructors
+
+    /// <summary>
+    /// This constructor creates a new instance of the <see cref="ApiException"/>
+    /// class.
+    /// </summary>
+    public ApiException()
+    {
+
+    }
+
+    // *******************************************************************
+
+    /// <summary>
+    /// This constructor creates a new instance of the <see cref="ApiException"/>
+    /// class.
+    /// </summary>
+    /// <param name="message">The message to use for the exception.</param>
+    /// <param name="innerException">An optional inner exception reference.</param>
+    public ApiException(
+        string message,
+        Exception innerException
+        ) : base(message, innerException)
+    {
+
+    }
+
+    // *******************************************************************
+
+    /// <summary>
+    /// This constructor creates a new instance of the <see cref="ApiException"/>
+    /// class.
+    /// </summary>
+    /// <param name="message">The message to use for the exception.</param>
+    public ApiException(
+        string message
+        ) : base(message)
+    {
+
+    }
+
+    #endregion
+}
---
+++ src/CodeGator.Exceptions/DirectorException.cs
@@ -0,0 +1,55 @@
+﻿
+namespace CodeGator.Exceptions;
+
+/// <summary>
+/// This class represents a director related exception.
+/// </summary>
+public class DirectorException : Exception
+{
+    // *******************************************************************
+    // Constructors.
+    // *******************************************************************
+
+    #region Constructors
+
+    /// <summary>
+    /// This constructor creates a new instance of the <see cref="DirectorException"/>
+    /// class.
+    /// </summary>
+    public DirectorException()
+    {
+
+    }
+
+    // *******************************************************************
+
+    /// <summary>
+    /// This constructor creates a new instance of the <see cref="DirectorException"/>
+    /// class.
+    /// </summary>
+    /// <param name="message">The message to use for the exception.</param>
+    /// <param name="innerException">An optional inner exception reference.</param>
+    public DirectorException(
+        string message,
+        Exception innerException
+        ) : base(message, innerException)
+    {
+
+    }
+
+    // *******************************************************************
+
+    /// <summary>
+    /// This constructor creates a new instance of the <see cref="DirectorException"/>
+    /// class.
+    /// </summary>
+    /// <param name="message">The message to use for the exception.</param>
+    public DirectorException(
+        string message
+        ) : base(message)
+    {
+
+    }
+
+    #endregion
+}
