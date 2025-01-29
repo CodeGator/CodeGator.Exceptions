83f0c1342562467e6c9369378041b288fcdd68db
Martin Cook <33993749+martycook@users.noreply.github.com>
2025-01-29 16:12:48 -0600

    I added a configuration exception type

---
+++ src/CodeGator.Exceptions/ConfigurationException.cs
@@ -0,0 +1,55 @@
+﻿
+namespace CodeGator.Exceptions;
+
+/// <summary>
+/// This class represents a configuration related exception.
+/// </summary>
+public class ConfigurationException : Exception
+{
+    // *******************************************************************
+    // Constructors.
+    // *******************************************************************
+
+    #region Constructors
+
+    /// <summary>
+    /// This constructor creates a new instance of the <see cref="ConfigurationException"/>
+    /// class.
+    /// </summary>
+    public ConfigurationException()
+    {
+
+    }
+
+    // *******************************************************************
+
+    /// <summary>
+    /// This constructor creates a new instance of the <see cref="ConfigurationException"/>
+    /// class.
+    /// </summary>
+    /// <param name="message">The message to use for the exception.</param>
+    /// <param name="innerException">An optional inner exception reference.</param>
+    public ConfigurationException(
+        string message,
+        Exception innerException
+        ) : base(message, innerException)
+    {
+
+    }
+
+    // *******************************************************************
+
+    /// <summary>
+    /// This constructor creates a new instance of the <see cref="ConfigurationException"/>
+    /// class.
+    /// </summary>
+    /// <param name="message">The message to use for the exception.</param>
+    public ConfigurationException(
+        string message
+        ) : base(message)
+    {
+
+    }
+
+    #endregion
+}
