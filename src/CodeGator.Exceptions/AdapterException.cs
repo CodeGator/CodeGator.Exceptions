df65d5241543fa7d3443cb1c67726da2ce204f14
Martin Cook <33993749+martycook@users.noreply.github.com>
2026-04-14 06:34:23 -0500

    I added AdapterException

---
+++ src/CodeGator.Exceptions/AdapterException.cs
@@ -0,0 +1,55 @@
+﻿
+namespace CodeGator.Exceptions;
+
+/// <summary>
+/// This class represents an adpater related exception.
+/// </summary>
+public class AdapterException : Exception
+{
+    // *******************************************************************
+    // Constructors.
+    // *******************************************************************
+
+    #region Constructors
+
+    /// <summary>
+    /// This constructor creates a new instance of the <see cref="AdapterException"/>
+    /// class.
+    /// </summary>
+    public AdapterException()
+    {
+
+    }
+
+    // *******************************************************************
+
+    /// <summary>
+    /// This constructor creates a new instance of the <see cref="AdapterException"/>
+    /// class.
+    /// </summary>
+    /// <param name="message">The message to use for the exception.</param>
+    /// <param name="innerException">An optional inner exception reference.</param>
+    public AdapterException(
+        string message,
+        Exception innerException
+        ) : base(message, innerException)
+    {
+
+    }
+
+    // *******************************************************************
+
+    /// <summary>
+    /// This constructor creates a new instance of the <see cref="AdapterException"/>
+    /// class.
+    /// </summary>
+    /// <param name="message">The message to use for the exception.</param>
+    public AdapterException(
+        string message
+        ) : base(message)
+    {
+
+    }
+
+    #endregion
+}
